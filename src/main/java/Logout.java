b7cb0833ebcbf855d95ecc6ef283c6e8a832bdc5
Patricia Lamadrid <cf18patricia.lamadrid@iesjoandaustria.org>
2020-04-30 20:45:30 +0200

    Login update Login with sessions.

---
+++ src/main/java/Logout.java
@@ -0,0 +1,33 @@
+package main.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		HttpSession session = request.getSession(false);
+		session.removeAttribute("user");
+		session.getMaxInactiveInterval();
+		response.sendRedirect("Home.jsp");
+	}
+
+}
