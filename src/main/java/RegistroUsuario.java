0b56b36a258713e71b25279ef251f1431fa052b4
Patricia Lamadrid <cf18patricia.lamadrid@iesjoandaustria.org>
2020-04-30 17:33:53 +0200

    RegistroUsuario.java Minor correction.

--- src/main/java/RegistroUsuario.java
+++ src/main/java/RegistroUsuario.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
 @WebServlet("/RegistroUsuario")
 public class RegistroUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final Logger LOGGER = Logger.getLogger("main.java.Login");
+	private static final Logger LOGGER = Logger.getLogger("main.java.RegistroUsuario");
        
     /**
      * @see HttpServlet#HttpServlet()
